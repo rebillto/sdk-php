4cb23ed9f4d8d21ea4e80b8196be3db96318018b
Kijam Lopez <kijamve@gmail.com>
2022-06-22 12:17:55 -0400

    Example for Update Subscription

---
+++ examples/get-set-subscription.php
@@ -0,0 +1,14 @@
+<?php
+
+include('setting.inc.php');
+
+$result = (new \Rebill\SDK\Models\Subscription)->get('07034a67-7c9d-406e-8b4d-8af4a9820b23');
+
+var_dump($result);
+
+$result->card = $result->card;
+$result->amount = $result->price->amount;
+$result->status = $result->status;
+$result->nextChargeDate = '2022-06-09T14:55:00.000Z';
+$result->update();
+var_dump($result);
